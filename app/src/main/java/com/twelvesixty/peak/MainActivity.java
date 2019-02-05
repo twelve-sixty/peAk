6481ab96b4f6a35958cf149d10a4230624eb9091
Darrin Howell <dbhowell09@gmail.com>
2019-02-05 15:39:04 -0800

    added pseudo nav to main activity; toggling between linear layouts

--- app/src/main/java/com/twelvesixty/peak/MainActivity.java
+++ app/src/main/java/com/twelvesixty/peak/MainActivity.java
@@ -3,7 +3,9 @@ package com.twelvesixty.peak;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onProfileButtonClick(View view) {
+        Intent navigateToProfile = new Intent(getApplicationContext(), UserProfileActivty.class);
+        startActivity(navigateToProfile);
+        setContentView(R.layout.activity_user_profile_activty);
     }
 }
--- app/src/main/java/com/twelvesixty/peak/UserProfileActivty.java
+++ app/src/main/java/com/twelvesixty/peak/UserProfileActivty.java
@@ -3,12 +3,56 @@ package com.twelvesixty.peak;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class UserProfileActivty extends AppCompatActivity {
+    LinearLayout defaultProfileLayout;
+    LinearLayout editableLayout;
+    Button editProfileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile_activty);
+
+        // find views by id
+        editableLayout = findViewById(R.id.linearLayout_editable);
+        defaultProfileLayout = findViewById(R.id.linearLayout_default);
+        editProfileButton = findViewById(R.id.button_EditProfile);
+
+        // set editable layout view to GONE
+        editableLayout.setVisibility(View.GONE);
+    }
+
+    public void onEditProfileButtonClick(View view) {
+
+        String buttonText = editProfileButton.getText().toString().toLowerCase();
+
+        if(buttonText.equals("edit profile")) {
+            flipToEditMode();
+            buttonText = "Save";
+            editProfileButton.setText(buttonText);
+
+        } else {
+            flipToDefaultMode();
+            buttonText = "Edit Profile";
+            editProfileButton.setText(buttonText);
+
+        }
+
+
+    }
+
+    public void flipToEditMode(){
+        defaultProfileLayout.setVisibility(View.GONE);
+        editableLayout.setVisibility(View.VISIBLE);
+    }
+
+    public void flipToDefaultMode(){
+        editableLayout.setVisibility(View.GONE);
+        defaultProfileLayout.setVisibility(View.VISIBLE);
     }
 }
