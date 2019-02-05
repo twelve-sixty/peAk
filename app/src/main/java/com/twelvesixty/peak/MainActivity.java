8312ee14b014ceabfe86ccba09008857f872e88a
Kevinrosales <kevinrosalesolvera@gmail.com>
2019-02-05 11:47:25 -0800

    drop downs created

--- app/src/main/java/com/twelvesixty/peak/MainActivity.java
+++ app/src/main/java/com/twelvesixty/peak/MainActivity.java
@@ -3,6 +3,8 @@ package com.twelvesixty.peak;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 import com.google.android.gms.maps.MapView;
 
@@ -13,6 +15,74 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MapView map = findViewById(R.id.mapView);
-
+        dropWeather();
+        dropResorts();
+        dropGroups();
     }
+
+//    public void onClick(View v) {
+//        final Handler handler = new Handler();
+//
+//        R.id.weatherDrop.setOnClickListener(new View.OnClickListener() {
+//       @Override
+//       public void onClick(View v) {
+//           // make textview Gone
+//           R.id.weather.setVisibility(View.GONE);
+//
+//           // perform action
+//           System.out.println("perform action");
+//
+//           // make textview disappear
+//           handler.postDelayed(new Runnable() {
+//               @Override
+//               public void run() {
+//                   R.id.weather.setVisibility(View.VISIBLE);
+//               }
+//           }, 2000);
+//       }
+//   });
+//    }
+
+    public void dropWeather() {
+        findViewById(R.id.weatherDrop).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // make textview visible
+                findViewById(R.id.weather).setVisibility(View.GONE);
+
+                // perform action
+                System.out.println("perform action");
+
+                // make textview disappear
+                findViewById(R.id.weather).setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    public void dropGroups() {
+        findViewById(R.id.groupsDrop).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.Filters).setVisibility(View.GONE);
+
+                System.out.println("perform action");
+
+                findViewById(R.id.Filters).setVisibility(View.VISIBLE);
+            }
+        });
+    }
+    public void dropResorts() {
+        findViewById(R.id.resortsInfoDrop).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.resortsInfo).setVisibility(View.GONE);
+
+                System.out.println("perform action");
+
+                findViewById(R.id.resortsInfo).setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+
 }
