a59e9d30394353b80527da415a2887c3e478fa5c
pablo <pablo.rosales14@gmail.com>
2019-02-06 13:32:07 -0800

    first layout is set, next one is for the default view

--- app/src/main/java/com/twelvesixty/peak/GroupDetailsActivity.java
+++ app/src/main/java/com/twelvesixty/peak/GroupDetailsActivity.java
@@ -3,12 +3,17 @@ package com.twelvesixty.peak;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class GroupDetailsActivity extends AppCompatActivity {
+    Button editGroupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_details);
+
+        // find views by id
+        editGroupButton = findViewById(R.id.editGroupButton);
     }
 }
