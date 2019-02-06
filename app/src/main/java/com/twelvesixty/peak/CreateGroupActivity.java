6e8972ee6b0c2e53184fbc0878f3f80385402fd7
Darrin Howell <dbhowell09@gmail.com>
2019-02-06 09:39:11 -0800

    building out logic to handle form data

--- app/src/main/java/com/twelvesixty/peak/CreateGroupActivity.java
+++ app/src/main/java/com/twelvesixty/peak/CreateGroupActivity.java
@@ -3,8 +3,14 @@ package com.twelvesixty.peak;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class CreateGroupActivity extends AppCompatActivity {
+    // group category checkboxes
+    CheckBox bluesAndBrews;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +18,12 @@ public class CreateGroupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_group);
     }
 
+    // on click method that takes in all of that data
+    // need to account for when users don't fill in certain fields
 
+    public void onCreateGroupButtonClick(View view){
+        // find all edit text views by ID
+        // create Group object with data
+        // send that data off to the backend in a post body
+    }
 }
