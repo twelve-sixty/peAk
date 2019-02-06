f8f33c54b8d7de26a53d7a48aa223effb595dd6c
Darrin Howell <dbhowell09@gmail.com>
2019-02-06 08:16:35 -0800

    first draft of group form

---
+++ app/src/main/java/com/twelvesixty/peak/CreateGroupActivity.java
@@ -0,0 +1,16 @@
+package com.twelvesixty.peak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_group);
+    }
+
+
+}
