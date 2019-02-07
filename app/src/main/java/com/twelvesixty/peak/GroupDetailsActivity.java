0932da9c9bcf1022262b069533489380fa6a29e8
pablo <pablo.rosales14@gmail.com>
2019-02-07 11:28:18 -0800

    Finished writing functionality, pushing changes to get feedback.

--- app/src/main/java/com/twelvesixty/peak/GroupDetailsActivity.java
+++ app/src/main/java/com/twelvesixty/peak/GroupDetailsActivity.java
@@ -27,55 +27,96 @@ public class GroupDetailsActivity extends AppCompatActivity {
         editableGroupLayout = findViewById(R.id.LinearLayout_GroupDetails);
 
         // if this user is the group admin, show the edit button
-//        if(The user owns this group){
+//        if(User.getOwnsGroup()){
 //          editGroupButton.setVisibility(View.VISIBLE);
 //        }
 
     }
 
-    // Need to migrate this method for this activity.
-    // Method shows users content from group or provides editable content for the owner
+    // Method shows users details about a group and can provide editable content for the owner to update
+    public void onEditGroupButtonClick(View view) {
 
-//    public void onEditGroupButtonClick(View view) {
-//
-//        String buttonText = editGroupButton.getText().toString().toLowerCase();
-//
-//        if(buttonText.equals("edit group")) {
-//
-//            flipToEditMode();
-//            buttonText = "Save";
-//            editGroupButton.setText(buttonText);
-//
-//        } else {
-//
-//            buttonText = "Edit group";
-//            editGroupButton.setText(buttonText);
-//
-//            EditText bioFormEditView = findViewById(R.id.textView_Bio_Editable);
-//            String bioFormInput = bioFormEditView.getText().toString();
-//
-//            EditText favoriteResortEditView = findViewById(R.id.textView_favoriteResort_Editable);
-//            String favoriteResortFormInput = favoriteResortEditView.getText().toString();
-//
-//            TextView bioTextView = findViewById(R.id.textView_Bio);
-//            TextView favoriteResortTextView = findViewById(R.id.textView_favoriteResort);
-//
-//            // if users provide content, reset the text in the user profile
-//            if(!bioFormInput.equals("")) {
-//                Log.i("bioFormInput", bioFormInput);
-//                bioTextView.setText(bioFormInput);
-//            }
-//
-//            // if users provide content for faveResort, then update textView
-//            if(!favoriteResortFormInput.equals("")) {
-//                Log.i("favoriteResortInput", favoriteResortFormInput);
-//                favoriteResortTextView.setText(favoriteResortFormInput);
-//            }
-//
-//            flipToDefaultMode();
-//
-//        }
-//    }
+        String buttonText = editGroupButton.getText().toString().toLowerCase();
+
+        if(buttonText.equals("edit group")) {
+
+            flipToEditMode();
+            buttonText = "Save";
+            editGroupButton.setText(buttonText);
+
+        } else {
+
+            buttonText = "Edit group";
+            editGroupButton.setText(buttonText);
+
+            //grabbing user inputs for each field
+            EditText groupNameFormEditView = findViewById(R.id.nameInputField);
+            String groupNameInput = groupNameFormEditView.getText().toString();
+
+            EditText meetingDateFormEditView = findViewById(R.id.dateInputField);
+            String meetingDateInput = meetingDateFormEditView.getText().toString();
+
+            EditText meetingTimeFormEditView = findViewById(R.id.timeInputField);
+            String meetingTimeInput = meetingTimeFormEditView.getText().toString();
+
+            EditText ResortFormEditView = findViewById(R.id.resortInputField);
+            String resortInput = ResortFormEditView.getText().toString();
+
+            EditText maximumCapacityFormEditView = findViewById(R.id.capacityInputField);
+            String maximumCapacityInput = maximumCapacityFormEditView.getText().toString();
+
+            EditText descriptionFormEditView = findViewById(R.id.descriptionInputField);
+            String descriptionInput = descriptionFormEditView.getText().toString();
+
+            EditText statusFormEditView = findViewById(R.id.statusInputField);
+            String statusInput = statusFormEditView.getText().toString();
+
+            //grabbing the default text views for updates
+            TextView groupNameTextView = findViewById(R.id.groupName);
+            TextView meetingDateAndTimeTextView = findViewById(R.id.dateLabel);
+            TextView resortTextView = findViewById(R.id.resortLabel);
+            TextView groupMaxCapacityTextView = findViewById(R.id.capacityLabel);
+            TextView groupDescriptionTextView = findViewById(R.id.descriptionLabel);
+            TextView groupStatusTextView = findViewById(R.id.stateLabel);
+
+            String timeAndDateToMeet = meetingDateInput + " at " + meetingTimeInput;
+
+            // if users provide content, reset the text in the appropriate fields
+            if(!groupNameInput.equals("")) {
+                Log.i("groupNameInput", groupNameInput);
+                groupNameTextView.setText(groupNameInput);
+            }
+
+            if(!meetingDateInput.equals("") || !meetingTimeInput.equals("")) {
+                Log.i("meetingDateInput", meetingDateInput);
+                Log.i("meetingTimeInput", meetingTimeInput);
+                meetingDateAndTimeTextView.setText(timeAndDateToMeet);
+            }
+
+            if(!resortInput.equals("")) {
+                Log.i("resortInput", resortInput);
+                resortTextView.setText(resortInput);
+            }
+
+            if(!maximumCapacityInput.equals("")) {
+                Log.i("maximumCapacityInput", maximumCapacityInput);
+                groupMaxCapacityTextView.setText(maximumCapacityInput);
+            }
+
+            if(!descriptionInput.equals("")) {
+                Log.i("descriptionInput", descriptionInput);
+                groupDescriptionTextView.setText(descriptionInput);
+            }
+
+            if(!statusInput.equals("")) {
+                Log.i("statusInput", statusInput);
+                groupStatusTextView.setText(statusInput);
+            }
+
+            flipToDefaultMode();
+
+        }
+    }
 
     // flip layouts from default content to editable content
     private void flipToEditMode() {
