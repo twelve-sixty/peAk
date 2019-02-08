3ec8df540a2f31b1dacede3126051742f43f3ba7
pablo <pablo.rosales14@gmail.com>
2019-02-07 18:23:08 -0800

    Creating a class to make a user and send to the backend

--- app/src/main/java/com/twelvesixty/peak/Signup.java
+++ app/src/main/java/com/twelvesixty/peak/Signup.java
@@ -3,6 +3,7 @@ package com.twelvesixty.peak;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Signup extends AppCompatActivity {
 
@@ -11,4 +12,20 @@ public class Signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
     }
+
+    EditText usernameInputField = findViewById(R.id.UserSignUp);
+    String usernameInput = usernameInputField.getText().toString();
+
+    EditText emailInputField = findViewById(R.id.EmailSignUp);
+    String emailInput = emailInputField.getText().toString();
+
+    EditText bioInputField = findViewById(R.id.BioSignUp);
+    String bioInput = bioInputField.getText().toString();
+
+    EditText passwordInputField = findViewById(R.id.PasswordSignUp);
+    String passwordInput = passwordInputField.getText().toString();
+
+    EditText dobInputField = findViewById(R.id.DOBSignUp);
+    String dobInput = dobInputField.getText().toString();
+
 }
